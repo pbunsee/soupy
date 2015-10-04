6580de3ebe49b2e5c14ed4ac34067a0ebb310d2c
Pranesha Bunsee <pbunsee@gmail.com>
2015-10-03 22:40:12 -0400

    Changed url - trimmed out unnecessary parameters

--- scraper_google_stocks.py
+++ scraper_google_stocks.py
@@ -1,7 +1,14 @@
 from bs4 import BeautifulSoup
 import urllib3
 
-url = "https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=aapl+stock+prices"
+# Note that urllib3 is not backwards compatible with urllib2 
+# opening the socket is done completely differently in urllib2
+
+# original url shown below but the sourceid and espv variables can be stripped out
+# want to keep the UTF-8 encoding in the GET request though for correct char handling
+# url = "https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=aapl+stock+prices"
+
+url = "https://www.google.com/webhp?ie=UTF-8#q=aapl+stock+prices"
 manager = urllib3.PoolManager()
 req = manager.request('GET', url)
 
